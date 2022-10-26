373c1dbbad16569e88eb76f1746fc632d4df901b
zbyju <zbynek.jurica@gmail.com>
2022-10-26 18:10:21 +0300

    Add serving basic frontend

--- node-express/index.js
+++ node-express/index.js
@@ -1,1 +1,13 @@
-console.log("test");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
