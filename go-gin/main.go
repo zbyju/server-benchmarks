c3af459875ae46e607339111d006dbb691961039
zbyju <zbynek.jurica@gmail.com>
2022-11-01 13:25:05 +0200

    Change docker to restart on change

--- go-gin/main.go
+++ go-gin/main.go
@@ -1,28 +1,87 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	gonanoid "github.com/matoous/go-nanoid"
 )
+
+type UrlRequestBody struct {
+	Url string
+}
+
+type Link struct {
+	url   string
+	bitly string
+}
+
+func linkToJson(link Link) map[string]any {
+	return map[string]any{
+		"url":   string(link.url),
+		"bitly": string(link.bitly),
+	}
+}
+
+func generateBitly() string {
+	id, err := gonanoid.Generate("abcdefghijklmnopqrstuvwxyz", 5)
+	if err != nil {
+		return "abcde"
+	}
+	return id
+}
+
+func deleteAll() {
+	fmt.Println("Deleting")
+}
+
+func saveUrl(url string) Link {
+	fmt.Println("Saving: ", url)
+	link := getLinkByUrl(url)
+	if link != nil {
+		return *link
+	}
+	bitly := generateBitly()
+	return Link{url, bitly}
+}
+
+func getRandom() Link {
+	fmt.Println("Getting random")
+	return Link{url: "http://google.com", bitly: "abcde"}
+}
+
+func getLinkByUrl(url string) *Link {
+	fmt.Println("Getting by URL: ", url)
+	return &Link{url: "http://google.com", bitly: "abcde"}
+}
+
+func getLinkByBitly(bitly string) *Link {
+	fmt.Println("Getting by bitly: ", bitly)
+	return &Link{url: "http://google.com", bitly: "abcde"}
+}
 
 func main() {
 	r := gin.Default()
 
 	r.DELETE("/api/url", func(c *gin.Context) {
+		deleteAll()
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
 	r.POST("/api/url", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		var requestBody UrlRequestBody
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.String(400, "No url")
+		}
+		link := saveUrl(requestBody.Url)
+		c.JSON(200, linkToJson(link))
 	})
 
 	r.POST("/random", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		link := getRandom()
+		c.JSON(200, linkToJson(link))
 	})
 
 	r.GET("/>bitly", func(c *gin.Context) {
@@ -35,5 +94,5 @@ func main() {
 	r.StaticFile("/js/main.js", "./public/js/main.js")
 	r.StaticFile("/css/styles.css", "./public/css/styles.css")
 
-	r.Run(":4000") // listen and serve on 0.0.0.0:8080
+	r.Run()
 }
